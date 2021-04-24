c2265ad42464d6b9536cd3ab42af52d32b02168e
Pilar <pilar.montes01@gmail.com>
2021-04-24 15:46:28 -0500

    Filter added

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,22 +6,44 @@ let tbody = d3.select("tbody");
 
 // This function is triggered when the "Filter Table" button is clicked
 function filterDate(){
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    // Get the date filter value
     let newDate = d3.select("#datetime").property("value");
     console.log(`New value: ${newDate}`);
+    // Define the Regex expression that the date should follow
+    let dateRegex = new RegExp("\\b(1\/([1-9]|1[0-3])\/2010)\\b");
+    // Verify if the value has a correct date format
+    if (dateRegex.test(newDate)) {
+        console.log("Correct Date format")
+        let filterSightings = tableData.filter(sighting => sighting.datetime === newDate);
+        //Erase the previous table
+        tbody.selectAll("tr").remove();
+        //Populate the table with filtered data
+        filterSightings.forEach(tableHandler);
+    } else {
+        console.log(`${newDate} is not between 1/1/2010 and 1/13/2010`)
+    }
 
 }
 
-// Populates the table from data.js 
-tableData.forEach(function(sighting){
-    console.log(sighting.datetime);
+// This funtions creates & modifies the table
+function tableHandler(sighting){
     let row = tbody.append("tr");
     Object.entries(sighting).forEach(function ([key, value]) {
         let cell = row.append("td");
         cell.text(value);
     });
+}
 
-});
+// Populates the table from data.js 
+tableData.forEach(tableHandler);
 
-
-// Event listener for the "Filter Table" button
+// Event listener for the "Filter Table" button & click enter on form
 d3.select("#filter-btn").on("click", filterDate);
+
+// d3.select("#datetime").on("keypress", function(event){
+//     if (event.keyCode === 13){
+//         filterDate;
+//     }
+// });
